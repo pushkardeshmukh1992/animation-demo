426edbf599053247dd75e4e8847f8e366f6b9539
Pushkar Deshmukh <pushkard@sminq.com>
2019-02-17 12:52:57 +0530

    Initial Commit

---
+++ labelShimmer/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  labelShimmer
+//
+//  Created by Pushkar Deshmukh on 17/02/19.
+//  Copyright © 2019 Pushkar Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        
+        let darkTextLabel = UILabel()
+        darkTextLabel.text = "Shimmer"
+        darkTextLabel.textColor = UIColor(white: 1, alpha: 0.2)
+        darkTextLabel.font = UIFont.systemFont(ofSize: 80)
+        darkTextLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
+        darkTextLabel.textAlignment = .center
+        view.addSubview(darkTextLabel)
+        
+        let shinyTextLabel = UILabel()
+        shinyTextLabel.text = "Shimmer"
+        shinyTextLabel.textColor = .white
+        shinyTextLabel.font = UIFont.systemFont(ofSize: 80)
+        shinyTextLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
+        shinyTextLabel.textAlignment = .center
+        view.addSubview(shinyTextLabel)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.white.cgColor, UIColor.clear.cgColor]
+        gradientLayer.locations = [0, 0.5, 1]
+        gradientLayer.frame = shinyTextLabel.frame
+        
+        let angle = 60 * CGFloat.pi / 180
+        gradientLayer.transform = CATransform3DMakeRotation(angle, 0, 0, 1)
+        
+        shinyTextLabel.layer.mask = gradientLayer
+        
+        // animation
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.fromValue = -view.frame.width
+        animation.toValue = view.frame.width
+        animation.repeatCount = Float.infinity
+        animation.duration = 2
+        
+        gradientLayer.add(animation, forKey: "doesnt matter")
+//        view.layer.addSublayer(gradientLayer)
+        
+    }
+
+
+}
+
